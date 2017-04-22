30399088e7d295172584933b6dd7a8ebeb6b05ca
Sandokk28 <edwin.sandoval@unillanos.edu.co>
2017-04-22 11:38:49 -0500

    Mas arreglos a los nombres de los modelos

--- app/PermisosUsuario.php
+++ app/PermisosUsuario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
-class Permisos_usuario extends Model implements AuditableContract
+class PermisosUsuario extends Model implements AuditableContract
 {
     use Auditable;
 
